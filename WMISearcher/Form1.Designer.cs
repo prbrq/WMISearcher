eff2581ab742ee812d1e38ec3fc2dbd65d6a3f92
prbrq <prbrq@ya.ru>
2018-06-29 12:25:50 +0300

    Initial commit

---
+++ WMISearcher/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WMITest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.WindowText;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 482);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.OnRowPrePaint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(154, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(918, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Domain\\Username:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(108, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.SystemColors.MenuText;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(386, 482);
+            this.dataGridView2.TabIndex = 7;
+            this.dataGridView2.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.OnRowPrePaint);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(128, 40);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(944, 482);
+            this.splitContainer1.SplitterDistance = 386;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 525);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1084, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IP:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 50);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Save to DB";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1084, 547);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WMISearcher";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WMISearcher/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMITest2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Сканирование запущено
+        /// </summary>
+        bool IsRunning = false;
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        [STAThread]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsRunning)
+                {
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    button2.Enabled = true;
+
+                    Cursor = Cursors.WaitCursor;
+
+                    //отображение информации о запущенных процессах
+                    dataGridView1.Columns.Clear();
+                    dataGridView1.Columns.Add("ProcessId", "ProcessId");
+                    dataGridView1.Columns.Add("Name", "Name");
+                    dataGridView1.Columns.Add("WindowsVersion", "WindowsVersion");
+                    dataGridView1.Columns.Add("VirtualSize", "VirtualSize");
+                    dataGridView1.Columns.Add("CSName", "CSName");
+
+                    //отображение информации о найденных в сети компьютерах
+                    dataGridView2.Columns.Clear();
+                    dataGridView2.Columns.Add("CSName", "CSName");
+                    dataGridView2.Columns.Add("IP", "IP");
+                    dataGridView2.Columns.Add("Description", "Description");
+                    dataGridView2.Columns.Add("MACAddress", "MACAddress");
+
+                    //в случае пустого адреса поле заполнится IP-адресом локального компьютера
+                    if (string.IsNullOrEmpty(textBox1.Text))
+                    {
+                        textBox1.Text = "127.0.0.1";
+                    }
+
+                    //обработка введённого в textBox1.text диапазона адрессов, которые хранятся в переменной addresses  
+                    string diapason = textBox1.Text;
+                    var sae = diapason.Split('-').ToList();
+                    if (sae.Count == 1)
+                    {
+                        sae.Add(sae[0]);
+                    }
+                    string diap1 = sae[0];
+                    string diap2 = sae[1];
+                    var array1 = diap1.Split('.');
+                    var array2 = diap2.Split('.');
+
+                    int start = BitConverter.ToInt32(new byte[]
+                    {
+                    Convert.ToByte(array1[3]),
+                    Convert.ToByte(array1[2]),
+                    Convert.ToByte(array1[1]),
+                    Convert.ToByte(array1[0])
+                    }, 0);
+                    int end = BitConverter.ToInt32(new byte[]
+                    {
+                    Convert.ToByte(array2[3]),
+                    Convert.ToByte(array2[2]),
+                    Convert.ToByte(array2[1]),
+                    Convert.ToByte(array2[0])
+                    }, 0);
+
+                    List<IPAddress> addresses = new List<IPAddress>();
+                    for (int i = start; i <= end; i++)
+                    {
+                        byte[] bytes = BitConverter.GetBytes(i);
+                        addresses.Add(new IPAddress(new[] { bytes[3], bytes[2], bytes[1], bytes[0] }));
+                    }
+
+                    foreach (var address in addresses)
+                    {
+                        if (TryPing(address))
+                        {
+                            GetWmi(address);
+                        }
+                        else
+                        {
+                            toolStripStatusLabel1.Text = $"{address} not available";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace, ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+                button2.Enabled = IsRunning = false;
+            }
+        }
+
+        private void GetWmi(IPAddress address)
+        {
+            
+
+            //подключение к компьютеру в сети
+            ManagementScope scope = new ManagementScope($@"\\{ address }\root\cimv2");
+            var username = textBox2.Text;
+            var password = textBox3.Text;
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                ConnectionOptions options = new ConnectionOptions();
+                options.Username = $@"{ textBox2.Text }";
+                options.Password = $@"{ textBox3.Text }";
+                scope.Options = options;
+            }
+            scope.Connect();
+            
+            var task1 = Task.Factory.StartNew(async delegate
+            {
+                // вывод в таблицу №2 информации о компьютерах в сети
+                const string QueryN = "SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = True";
+                ManagementObjectSearcher networkEnumerator = new ManagementObjectSearcher(QueryN);
+                networkEnumerator.Scope = scope;
+                foreach (ManagementObject NetEd in networkEnumerator.Get())
+                {
+                    var Hname = NetEd["DNSHostName"];
+                    var Desc = NetEd["Description"];
+                    var array = NetEd["IPAddress"] as string[];
+                    var IPA = string.Join(", ", array);
+                    var MACA = NetEd["MACAddress"];
+                    await Task.Run(delegate
+                    {
+                        dataGridView2.Invoke(new Action(delegate
+                        {
+                            dataGridView2.Rows.Add(Hname, IPA, Desc, MACA);
+                        }));
+                    });
+                }
+            }, cts);
+
+            var task2 = Task.Factory.StartNew(async delegate
+            {
+                //вывод в таблицу №1 информации о процессах
+                const string Query = "SELECT * FROM Win32_Process";
+                ManagementObjectSearcher processEnumerator = new ManagementObjectSearcher(Query);
+                processEnumerator.Scope = scope;
+                foreach (ManagementObject process in processEnumerator.Get())
+                {
+                    var pid = process["ProcessId"];
+                    var name = process["Name"];
+                    var winver = process["WindowsVersion"];
+                    var vsize = process["VirtualSize"];
+                    var csn = process["CSName"];
+
+                    await Task.Run(delegate
+                    {
+                        dataGridView1.Invoke(new Action(delegate
+                        {
+                            dataGridView1.Rows.Add(pid, name, winver, vsize, csn);
+                        }));
+                    });
+
+                    if (checkBox1.Checked)
+                    {
+                        //подключение к базе данных
+                        SqlConnection conn = new SqlConnection();
+                        conn.ConnectionString = @"Server=localhost\SQLEXPRESS;Database=WMIDB;Trusted_Connection=True;";
+                        conn.Open();
+
+                        //очистка базы данных
+                        const string QueryD = "DELETE FROM dbo.Win32_Process";
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = QueryD;
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        //добавление информации о полученных процессах в базу данных
+                        const string Query1 = "INSERT INTO dbo.Win32_Process VALUES (@pid, @name, @winver, @vsize, @csn)";
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = Query1;
+                            cmd.Parameters.Add(new SqlParameter("@pid", SqlDbType.NVarChar) { Value = pid });
+                            cmd.Parameters.Add(new SqlParameter("@name", SqlDbType.NVarChar) { Value = name });
+                            cmd.Parameters.Add(new SqlParameter("@winver", SqlDbType.NVarChar) { Value = winver });
+                            cmd.Parameters.Add(new SqlParameter("@vsize", SqlDbType.NVarChar) { Value = vsize });
+                            cmd.Parameters.Add(new SqlParameter("@csn", SqlDbType.NVarChar) { Value = csn });
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        var row = $"{pid};{name};{winver};{vsize};{csn}";
+                        using (var writer = new StreamWriter("output.csv", true, Encoding.UTF8))
+                        {
+                            writer.WriteLine($@"{DateTime.Now}: {row}");
+                        }
+                    }
+                }
+            }, cts);
+
+            Task.WaitAll(task1, task2);
+        }
+
+        // функция, используемая для проверки доступности IP-адресов
+        public static bool TryPing(IPAddress address)
+        {
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send(address);
+            return reply.Status == IPStatus.Success;
+        }
+
+        private void OnRowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            var dgv = sender as DataGridView;
+            var ri = e.RowIndex;
+            if (ri > -1 && ri < dgv.RowCount)
+            {   // Черезстрочно закрашиваем в светлосерый
+                dgv.Rows[ri].DefaultCellStyle.BackColor = (ri & 1) == 0 ? Color.LightGray : Color.LightGreen;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+
+    }
+}
